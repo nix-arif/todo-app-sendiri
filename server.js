918ac534f40e8e14fec151359eb52e6624454aad
nixarif <nix.arif@gmail.com>
2020-04-17 16:38:21 +0800

    Changes to be committed:

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ let http = require('http')
 
 let ourApp = http.createServer(function(req, res) {
     if (req.url == '/') {
-        res.end('Hello arif')
+        res.end('Hello Arif')
     }
     if (req.url == '/about') {
-        res.end('Hai')
+        res.end('Thank you')
     }
     
 })
-
 
 ourApp.listen(3000)
