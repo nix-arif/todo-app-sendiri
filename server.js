d77e99917392ca8cd715ffbc025f466b1c511dd3
nixarif <nix.arif@gmail.com>
2020-04-18 12:25:18 +0800

    training

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 let http = require('http')
 
 let ourApp = http.createServer(function(req, res) {
-    if (req.url == '/') {
-        res.end('Hello Arif')
-    }
-    if (req.url == '/about') {
-        res.end('Thank you')
-    }
-    
+    res.end('Hello')
 })
 
 ourApp.listen(3000)
